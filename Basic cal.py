81b7c7eff039819ec718821d1ec5cd8de585232c
Lokesh Joshi <69955726+lokeshjoshi01@users.noreply.github.com>
2020-08-20 13:28:03 +0530

    Add files via upload

---
+++ Basic cal.py
@@ -0,0 +1,6 @@
+print("enter the number")
+n1 = input()
+print("enter the second number")
+n2 = input()
+print("add both numbers", int(n1)+int(n2))
+#add both ni jgya a game e lay skay jem k devision multiplicatin
